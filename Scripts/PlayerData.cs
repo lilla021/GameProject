5169dc57bb15edf68cd3439006a693795c2ad385
RedKroker <31494194+RedKroker@users.noreply.github.com>
2019-10-28 19:44:51 -0400

    Add files via upload

---
+++ Scripts/PlayerData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerData
+{
+    public static int CurrentHP { get; set; } = 100;
+    public static int MaxHP { get; set; } = 100;
+    public static int CurrentMana { get; set; } = 100;
+    public static int MaxMana { get; set; } = 100;
+    public static int CurrentXP { get; set; } = 0;
+    public static int MaxXP { get; set; } = 100;
+    public static int Level { get; set; } = 1;
+    public static int AbilityPoints { get; set; } = 0;
+    public static int PhysicalPower { get; set; }
+    public static int AbilityPower { get; set; }
+    public static float DreamTimerMaxValue { get; set; } = 30f;
+    public static float DreamTimerCurrentValue { get; set; } = 30f;
+    public static bool IsInDream { get; set; } = false;
+
+}
---
+++ Scripts/StatsMenuTextDisplayScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsMenuTextDisplayScript : MonoBehaviour
+{
+    public GameObject StatsMenu;
+
+    public Text HealthText;
+    public Image HealthBar;
+    public Text ManaText;
+    public Image ManaBar;
+
+    public
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            StatsMenu.SetActive(!StatsMenu.activeInHierarchy);
+        }
+
+        HealthText.text = "Health \t" + PlayerData.CurrentHP + " / " + PlayerData.MaxHP;
+        HealthBar.fillAmount = PlayerData.CurrentHP / PlayerData.MaxHP;
+        ManaText.text = "Mana \t" + PlayerData.CurrentMana + " / " + PlayerData.MaxMana;
+    }
+}
+
