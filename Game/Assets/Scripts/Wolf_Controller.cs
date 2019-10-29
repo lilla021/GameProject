44efc0faa1e844411125e11fe7c5c5000b71b5cc
Unknown <johnny.cl97@gmail.com>
2019-10-28 20:29:28 -0400

    Added some player animations, movement and 1 enemy

---
+++ Game/Assets/Scripts/Wolf_Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wolf_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.collider.name == "Player")
+        {
+            Debug.Log("Ouch");
+            PlayerData.CurrentHP = PlayerData.CurrentHP-5f;
+        }
+
+    }
+}
