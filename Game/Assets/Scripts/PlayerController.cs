fae3348aecf7253602fd25a35980213aebcff8f6
Mauricio Beltran Franco <mauricio.beltranfranco@live.fr>
2019-10-30 04:31:54 -0400

    Movement Upgrade
    Upgraded movement:
    -Ground Check is now working, did not use the ground check script, might want to delete it.
    -Added the ability to double jump (normally disabled until unlocked)
    -Added the ability to dash by pressing the shift key (normally disabled until unlocked). The dashing is not completed, this is just a base version. Do not dash on a slope; physics makes you go flying.
    -Changed some animations and added a fall animation : Jump is now smoother and faster, new Fall animation after jumping or going off a ledge, Attack is now smoother and blocks movement until the animation is over, Run and Idle are now smoother. Transitions upgraded.

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -11,9 +11,15 @@ public class PlayerController : MonoBehaviour {
     Vector3 currentVelocity = Vector3.zero;
     Vector3 targetVelocity;
     bool isJumping = false;
-    bool isRising;
+    bool isAttack = false;
+    bool isDashing = false;
+    bool dash = true;                  //Boolean variable to store whether the player unlocked the dash. (true for testing)
+    bool doubleJump = true;            //Boolean variable to store whether the player unlocked the double jump. (true for testing)
+    int extraJump = 1;                 //Integer variable to store the player's extra jump left.
     [SerializeField]
     float jumpForce;                   //Floating point variable to store the player's jump force.
+    [SerializeField]
+    float dashForce;
     [SerializeField]
     Transform groundCheck;
     bool isGrounded = false;
@@ -32,7 +38,6 @@ public class PlayerController : MonoBehaviour {
     void Update() {
         //Store the current horizontal input in the float moveHorizontal.
         moveHorizontal = Input.GetAxisRaw("Horizontal");
-        Attack();
         //Flip the sprite when necessary.
         if (moveHorizontal != 0) {
             transform.localScale = new Vector2(moveHorizontal, 1);           
@@ -42,6 +47,7 @@ public class PlayerController : MonoBehaviour {
         {
             mAnimator.SetBool("isRunning", false);
         }
+        Attack();
 
         //Store the current vertical input in the float moveVertical.
         //moveVertical = Input.GetAxisRaw("Vertical");
@@ -50,43 +56,48 @@ public class PlayerController : MonoBehaviour {
         //movement = new Vector2(moveHorizontal, moveVertical);
 
         isJumping = Input.GetButtonDown("Jump");
+        isDashing = Input.GetButtonDown("Dash");
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate() {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.1f, LayerMask.GetMask("Ground"));
+        isGrounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.35f, LayerMask.GetMask("Ground"));
         for (int i = 0; i < colliders.Length; i++) {
             if (colliders[i].gameObject != gameObject) {
                 isGrounded = true;
+                extraJump = 1;
             }
         }
+        mAnimator.SetBool("isGrounded", isGrounded);
+
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         //player.AddForce(movement * speed);
+        if (!isAttack) {
+            currentSpeed = moveHorizontal * maxSpeed;
+            targetVelocity = new Vector2(currentSpeed * Time.fixedDeltaTime, player.velocity.y);
+            player.velocity = Vector3.SmoothDamp(player.velocity, targetVelocity, ref currentVelocity, 0.04f);
+        }
 
-        currentSpeed = moveHorizontal * maxSpeed;
-        targetVelocity = new Vector2(currentSpeed * Time.fixedDeltaTime, player.velocity.y);
-        player.velocity = Vector3.SmoothDamp(player.velocity, targetVelocity, ref currentVelocity, 0.04f);
-
-        if (isJumping) {
-            mAnimator.SetBool("isRising", true);
+        if (isJumping && (isGrounded || (extraJump != 0 && doubleJump)) && !isAttack) {
+            if (!isGrounded) extraJump = 0;
+            mAnimator.Play("Jump");
+            player.velocity = new Vector2(player.velocity.x, 0);
             player.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isJumping = false;
         }
-        else
-        {
-            mAnimator.SetBool("isRising", false);
+
+        if (isDashing) {
+            player.AddForce(Vector2.right * transform.localScale.x * dashForce, ForceMode2D.Impulse);
+            isDashing = false;
         }
     }
 
     void Attack()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            mAnimator.SetBool("isAttack", true);
-        }
-        else
-        {
-            mAnimator.SetBool("isAttack", false);
+        if (!mAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack")) {
+            isAttack = Input.GetMouseButtonDown(0) ? true : false;
+            mAnimator.SetBool("isAttack", isAttack);
         }
     }
 }
