a589d257116a75b953f16a56c5cb743d60c6059e
dinasadir <45443818+dinasadir@users.noreply.github.com>
2019-10-29 21:33:14 -0400

    First 5 Rooms
    - Added DestroyMask script
    - Added 5 rooms to level

---
+++ Game/Assets/Scripts/DestroyMask.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMask : MonoBehaviour
+{
+    Renderer m_Renderer;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (m_Renderer.isVisible && PlayerData.IsInDream)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
