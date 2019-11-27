f1ad643236f352a88a03e0754e81e19834640e38
dinasadir <45443818+dinasadir@users.noreply.github.com>
2019-11-27 17:44:23 -0500

    Forest Level Update

---
+++ Game/Assets/Scripts/MoveWithPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithPlatform : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "Platform")
+        {
+            
+            transform.parent = col.transform.parent;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Platform")
+        {
+            transform.parent = null;
+        }
+    }
+}
---
+++ Game/Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    [SerializeField]
+    float spawnRateMin;
+    [SerializeField]
+    float spawnRateMax;
+
+    private float spawningRate;
+    private float next = 0;
+
+    GameObject spawner;
+
+    [SerializeField]
+    float maxSpawnOffsetX;
+    [SerializeField]
+    float maxSpawnOffsetY;
+    [SerializeField]
+    float maxSpawnOffsetZ;
+
+    [SerializeField]
+    GameObject spawnPrefab;
+
+    void Start()
+    {
+        if (gameObject.CompareTag("RockManager"))
+        {
+            spawner = GameObject.FindGameObjectWithTag("RockSpawner");
+        }
+        /*
+        if (gameObject.CompareTag("EnemyManager"))
+        {
+            treasureSpawner = GameObject.FindGameObjectWithTag("EnemySpawner");
+        }
+        */
+
+    }
+
+    void Update()
+    {
+
+        // Spawn a new treasure
+        if (Time.time > next)
+        {
+            float spawnX = Random.Range(-maxSpawnOffsetX, maxSpawnOffsetX);
+            float spawnY = Random.Range(-maxSpawnOffsetY, maxSpawnOffsetY);
+            float spawnZ = Random.Range(-maxSpawnOffsetZ, maxSpawnOffsetZ);
+            Vector3 offset = new Vector3(spawnX, spawnY, spawnZ);
+            Vector3 spawnLocation = spawner.transform.position + offset;
+
+            GameObject prefab = Instantiate(spawnPrefab, spawnLocation, Quaternion.identity);
+
+            spawningRate = Random.Range(spawnRateMin, spawnRateMax);
+            next = Time.time + spawningRate;
+        }
+
+    }
+
+}
