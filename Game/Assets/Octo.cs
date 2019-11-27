26e2f3e572b94c15a09ff796873b088335da55b8
Priscilla Cournoyer <p_courn@encs.concordia.ca>
2019-11-26 22:08:16 -0500

    okkkkkk mothafuckasssss

---
+++ Game/Assets/Octo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Octo : MonoBehaviour
+{
+    [SerializeField]
+    float mForce;
+
+    [SerializeField]
+    //float mOctoSpeed;
+
+    Rigidbody2D octo;
+
+    Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        octo = GetComponent<Rigidbody2D>();
+        startPosition = octo.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //transform.Translate(0f, Time.deltaTime, 0f);
+        if (octo.transform.position.y <= startPosition.y)  //makes player jump
+        {
+
+            octo.AddForce(new Vector2(0, 6), ForceMode2D.Impulse);
+            
+        }
+    }
+
+   
+}
