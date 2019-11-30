d471746ddde3f39297e00e38b3a8522ae52efe6e
unknown <charlotguite@hotmail.fr>
2019-11-30 04:53:42 -0500

    added main menu

---
+++ Game/Assets/Scripts/MainMenuCheatButtons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCheatButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject CheatButtons;
+       
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("y"))
+        {
+            CheatButtons.SetActive(!CheatButtons.activeSelf);
+        }
+    }
+}
