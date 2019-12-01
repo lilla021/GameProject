eb9f81deaec9f8c05ce0fb6f739ba44d23bebcbe
lilla021 <priscilla.cournoyer@hotmail.com>
2019-12-01 15:54:49 -0500

    Created both bos scene rooms, and tutorial room. Dialog instructions under construction!

---
+++ Game/Assets/Scripts/DialogPanelController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogPanelController : MonoBehaviour
+{
+
+    Text moveInstructions;
+    Text jumpInstructions;
+    Text dreamWorldInstructions;
+    Text inGameMenuInstructions;
+    Text mainMenuInstructions;
+
+    public bool moveInst;
+    public bool jumpInst;
+    public bool dreamInst;
+    public bool inGameMenuInst;
+    public bool mainMenuInst;
+    void Start()
+    {
+        Text[] allDialogTexts = GetComponents<Text>();
+        moveInst = allDialogTexts[0];
+        jumpInst = allDialogTexts[1];
+        dreamInst = allDialogTexts[2];
+        inGameMenuInst = allDialogTexts[3];
+        mainMenuInst = allDialogTexts[4];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveInst) { }; 
+        if (jumpInst) { }; 
+        if (dreamInst) { }; 
+        if (inGameMenuInst) { }; 
+        if (mainMenuInst) { }; 
+    }
+}
---
+++ Game/Assets/Scripts/DialogTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    public Dialog dialog;
+    private void Start()
+    {
+        TriggerDialog();
+    }
+
+    public void TriggerDialog()
+    {
+        FindObjectOfType<DialogManager>().StartDialog(dialog);
+    }
+}
